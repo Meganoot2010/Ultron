1a2b8836adafbd6461edf1ed4754345b4ff36961
Mega <cesardemussi@gmail.com>
2026-09-25 17:42:50 -0300

    Corrigindo problema de import

--- Ultron.java
+++ Ultron.java
@@ -1,3 +1,61 @@
+<<<<<<< HEAD
+package Ultron;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Ultron - a robot by (your name here)
+ */
+public class Ultron extends Robot
+{
+	/**
+	 * run: Ultron's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
+=======
 package mrvl;
 import robocode.*;
 //import java.awt.Color;
@@ -54,3 +112,4 @@ public class Ultron extends Robot
 		back(20);
 	}	
 }
+>>>>>>> 2e5b3b9ec4b8524fb4f476df51bef741ad32b113
